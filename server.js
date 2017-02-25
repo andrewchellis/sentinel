0925b4d1581af5f52285c16c132e851c7c605c4a
Andrew Chellis <andrewchellis@users.noreply.github.com>
2017-02-25 18:49:55 -0500

    Getting up to speed

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const db = new sqlite3.Database('sentinel.db');
 const morgan = require('morgan');
+const request = require('request');
+const helpers = require('./helpers.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true} ));
 app.use(express.static(__dirname));
+
+const quandl = "https://www.quandl.com/api/v3/datasets/WIKI/";
 
 app.use(morgan('dev'));
 
@@ -14,6 +18,27 @@ const port = 1337;
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname+'/index.html');
+});
+
+app.post('/api/graphCalc', (req,res) => {
+	let resp = {}, date;
+
+	req.body.tickers.foreach((ticker) => {
+		request.get(quandl+ticker+".json?column_index=4&start_date="+req.body.start+"&end_date="+req.body.end+"&collapse=daily&api_key="+helpers.api_key,
+			function(err,response,body){
+				if(error){
+					console.log(error);
+				}
+				else{
+					console.log(response);
+					console.log(body);
+				}
+			}
+		});
+	});
+
+
+	res.json(resp);
 });
 
 app.listen(port, () => {
